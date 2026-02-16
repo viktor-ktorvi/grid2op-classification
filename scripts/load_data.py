543e8af4849960e39caa8dc4f16b52fbb241201f
Viktor Todosijevic <todosijevicviktor998@gmail.com>
2026-02-16 23:39:52 +0100

    draw graph of pyg data

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,4 +1,7 @@
+import networkx as nx
 import numpy as np
+from matplotlib import pyplot as plt
+from torch_geometric.utils import to_networkx
 
 from src import DATA_PATH
 from src.dataset import load_data_sample, load_env, to_pyg_data
@@ -17,6 +20,26 @@ def main() -> None:
     data_sample = load_data_sample(filename)
 
     data = to_pyg_data(data_sample, env)
+
+    G = to_networkx(data)
+
+    pos = nx.nx_pydot.pydot_layout(G, prog="neato")
+
+    node_colors = []
+    for node in range(data.num_nodes):
+        if data.substation_indicator[node]:
+            node_colors.append("lightblue")
+
+        if data.gen_indicator[node]:
+            node_colors.append("lime")
+
+        if data.load_indicator[node]:
+            node_colors.append("orange")
+
+    fig, axs = plt.subplots(1, 1)
+
+    nx.draw(G, pos, with_labels=True, node_color=node_colors, node_size=500, ax=axs)
+    plt.show()
 
     print(data)
 
